548e9fdd00eac1de9962170dea69fbe383ec980a
khabibur607 <mkhrohman@gmail.com>
2025-01-08 11:14:59 +0700

    Add files via upload

---
+++ misi.js
@@ -0,0 +1,15 @@
+let nama = "Khabibur";
+let alamat = "njeroto rt 03 rw 07";
+let usia = "16";
+let ttl = "18 Desember 2008";
+let gender = "Pria";
+let agama = "Islam";
+let email = "macannyangkut@gmail.com";
+let berat = "95";
+let tinggi = "172";
+let hobi = "membaca";
+let cita = "Gamer";
+let sts = "pelajar";
+let sklh = "Smk N 1 Sayung";
+let jrsn = "PPLG";
+document.write(" nama saya adalah " + nama + " saya tinggal di " + alamat + " Pada tahun 2024 ini usia saya adalah " + usia + " tahun. Saya lahir tanggal " + ttl + " Jenis kelamin saya adalah " + gender + " saya beragama " + agama + " Alamat email saya adalah " + email + " Saya memiliki berat badan " + berat + " kg dengan tinggi " + tinggi + " cm. Saya sangat senang " + hobi + " karena memiliki cita-cita " + cita + " saat ini saya seorang " + sts + " yang bersekolah di " + sklh + " pada jurusan " + jrsn); 
